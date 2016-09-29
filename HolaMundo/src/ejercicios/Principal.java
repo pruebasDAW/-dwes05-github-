c82edc44ff322b524d33a4014d74ec29682e4e2f
alumno <alumno@w701>
2016-09-29 09:09:05 +0200

    Fichero modificado

--- HolaMundo/src/ejercicios/Principal.java
+++ HolaMundo/src/ejercicios/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Haciendo pruebas con commit y push");
 	}
 
 }
